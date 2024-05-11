caa5f8eb280d7572005e02febf79ac3d842621ba
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2024-05-12 01:11:01 +0200

    Base for future plugins

---
+++ Utilities/src/main/java/me/emiljoo/minecraftplugins/utilities/EnhancedPlugin.kt
@@ -0,0 +1,7 @@
+package me.emiljoo.minecraftplugins.utilities
+
+import org.bukkit.plugin.java.JavaPlugin
+
+abstract class EnhancedPlugin : JavaPlugin() {
+    abstract fun getPluginPrefix(): String
+}
