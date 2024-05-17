d3e0303677dae61ab1bc578d7ed4fb4f74c8c7d7
Emilj00 <106914054+Emilj00@users.noreply.github.com>
2024-05-17 22:02:42 +0200

    Save part of inventory on death

---
+++ Basic/src/main/kotlin/me/emiljoo/minecraftplugins/basic/modules/InventorySaver.kt
@@ -0,0 +1,43 @@
+package me.emiljoo.minecraftplugins.basic.modules
+
+import me.emiljoo.minecraftplugins.utilities.EnhancedPlugin
+import me.emiljoo.minecraftplugins.utilities.modules.EnhancedModule
+import org.bukkit.event.EventHandler
+import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.inventory.ItemStack
+import kotlin.random.Random
+
+
+class InventorySaver : EnhancedModule() {
+    override fun onEnable(plugin: EnhancedPlugin) {
+
+    }
+
+    override fun onDisable(plugin: EnhancedPlugin) {
+    }
+
+    @EventHandler
+    private fun onPlayerDeath(event: PlayerDeathEvent) {
+        // It's confusing asf.
+        // it keeps your inventory on death but also
+        // drops everything what is inside your inventory
+        event.keepInventory = true
+
+        val player = event.entity
+
+        val drops: MutableList<ItemStack> = event.drops
+        val inventory = player.inventory.contents
+
+        for (i in inventory.indices) {
+            if (inventory[i] != null) {
+                if (Random.nextBoolean()) { //TODO: Set it for percentage and Config it
+                    drops.remove(inventory[i])
+                } else {
+                    inventory[i] = null
+                }
+            }
+        }
+
+        player.inventory.contents = inventory
+    }
+}
